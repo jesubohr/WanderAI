fd290ec9016f0dd11d03cd7ec078aa387b915ae1
Jesus Borrero <jesufer2018@hotmail.com>
2024-12-13 22:57:19 -0500

    update consts

---
+++ src/lib/consts.ts
@@ -0,0 +1,37 @@
+export const BANNED_COUNTRIES = [
+  "Russia",
+  "Iran",
+  "Myanmar",
+  "Sudan",
+  "Iraq",
+  "Afghanistan",
+  "Ukraine",
+  "Yemen",
+  "Venezuela",
+  "North Korea",
+  "Syria",
+  "Mali",
+  "Burkina Faso",
+  "Somalia",
+  "Haiti",
+  "South Sudan",
+  "Belarus",
+  "Libya",
+  "Central African Republic"
+]
+
+export const CATEGORY_DESCRIPTIONS = {
+  adults: "Age 13 or above",
+  children: "Age 2-12",
+  infants: "Under 2",
+  pets: "*Service animals are welcomed!"
+}
+
+export const REGIONS = [
+  "I feel lucky!",
+  "Europe",
+  "Latin America",
+  "North America",
+  "Asia",
+  "Oceania",
+]
